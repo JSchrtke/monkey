225d738b40d37f17a32f11fcfa947578eb9fac3b
JSchrtke <jschrtke@protonmail.com>
2024-02-04 18:22:27 +0100

    wip: start implementing 'NextToken' method

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,15 +2,67 @@ package lexer
 
 import "monkey/token"
 
-type Lexer struct {}
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	currentChar  byte
+}
 
 func MakeLexer(input string) *Lexer {
-	return nil
+	lexer := &Lexer{input: input}
+	lexer.readChar()
+	return lexer
 }
 
 func (self *Lexer) NextToken() token.Token {
-	return token.Token {
-		Type: token.ILLEGAL,
-		Literal: "",
+	var tok token.Token
+
+	switch self.currentChar {
+	case '=':
+		tok = newToken(token.ASSING, self.currentChar)
+	case '+':
+		tok = newToken(token.PLUS, self.currentChar)
+	case ',':
+		tok = newToken(token.COMMA, self.currentChar)
+	case ';':
+		tok = newToken(token.SEMICOLON, self.currentChar)
+	case '(':
+		tok = newToken(token.LPAREN, self.currentChar)
+	case ')':
+		tok = newToken(token.RPAREN, self.currentChar)
+	case '{':
+		tok = newToken(token.LBRACE, self.currentChar)
+	case '}':
+		tok = newToken(token.RBRACE, self.currentChar)
+	case 0:
+		tok = newToken(token.EOF, 0)
+	default:
+		if isLetter(self.currentChar) {
+			// todo
+		} else {
+			tok = newToken(token.ILLEGAL, self.currentChar)
+		}
 	}
+
+	self.readChar()
+	return tok
+}
+
+func isLetter(b byte) bool {
+	return 65 <= b && b <= 90 || 97 <= b && b <= 122
+}
+
+func newToken(tokenType token.TokenType, char byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(char)}
+}
+
+func (self *Lexer) readChar() {
+	if self.readPosition >= len(self.input) {
+		self.currentChar = 0
+	} else {
+		self.currentChar = self.input[self.readPosition]
+	}
+	self.position = self.readPosition
+	self.readPosition += 1
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,17 +6,59 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := "=+(){},;"
+	input := `let five = 5;
+	let ten = 10;
 
-	expectedTokens := []token.Token {
+	let add = fn(x, y) {
+		x + y;
+	};
+
+	let result = add(five, ten);
+	`
+
+	expectedTokens := []token.Token{
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "five"},
 		{Type: token.ASSING, Literal: "="},
-		{Type: token.PLUS, Literal: "+"},
+		{Type: token.INT, Literal: "5"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.ASSING, Literal: "="},
+		{Type: token.INT, Literal: "10"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.ASSING, Literal: "="},
+		{Type: token.FUNCTION, Literal: "fn"},
 		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "y"},
 		{Type: token.RPAREN, Literal: ")"},
 		{Type: token.LBRACE, Literal: "{"},
-		{Type: token.RBRACE, Literal: "}"},
-		{Type: token.COMMA, Literal: ","},
+
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.PLUS, Literal: "+"},
+		{Type: token.IDENT, Literal: "y"},
 		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.RPAREN, Literal: "}"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "result"},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "five"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.EOF, Literal: ""},
 	}
 
 	lexer := MakeLexer(input)
